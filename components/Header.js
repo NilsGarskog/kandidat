8270fca6303c6b918a52edf9e46bd6e64a236c9f
NilsGarskog <93979268+NilsGarskog@users.noreply.github.com>
2023-04-04 10:05:17 +0200

    FixLogOutForNonUsers

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import Modal from './Modal'
+import { useAuth } from '../context/authContext'
 
 export default function Header() {
   const [openModal, setOpenModal] = useState(false)
+  const { currentUser } = useAuth()
 
-
-
+  if (!currentUser) {
+    return (
+      <>
+        <div className='sticky top-0 w-full left-0 bg-inherit flex items-center justify-between p-4 border-b border-solid border-white'>
+          <h1 className='text-3xl select-none sm:text-6xl font-latoBold'> TRAVEL PLANNER </h1>
+        </div>
+      </>
+    )
+  }
 
   return (
     <>
