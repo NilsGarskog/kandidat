001b242142807570db27687be18218cf26a756e4
NilsGarskog <93979268+NilsGarskog@users.noreply.github.com>
2023-04-12 13:03:04 +0200

    RuSurePopUp

--- components/UserDashboard.js
+++ components/UserDashboard.js
@@ -15,31 +15,44 @@ export default function UserDashboard() {
   const [trip, setTrip] = useState('')
   const [arrDate, setArrDate] = useState(null)
   const [depDate, setDepDate] = useState(null)
+  const [open, setOpen] = useState(false);
+  const [err, setErr] = useState("");
+
+  const contentStyle = { borderRadius: '20px' };
+  const overlayStyle = { background: 'rgba(0,0,0,0.5)' };
 
   const { trips, setTrips, loading, error } = useFetchTrips()
 
   async function handleAddTrip() {
-    if (!trip) { return }
+    if (!trip || arrDate === null || depDate === null) { return setErr('Please enter both dates') }
     const newKey = Object.keys(trips).length === 0 ? 1 : Math.max(...Object.keys(trips)) + 1
     setTrips({ ...trips, [newKey]: trip })
     const userRef = doc(db, 'users', currentUser.uid, 'Trips', newKey.toString())
     console.log(newKey)
     await setDoc(userRef, { Name: trip, arrDate: arrDate.format('YYYY-MM-DD'), depDate: depDate.format('YYYY-MM-DD') })
-    setTrip('')
-    setArrDate(null)
-    setDepDate(null)
+    handleButton()
 
   }
 
-  function handleDelete(tripKey) {
-    return async () => {
-      const tempObj = { ...trips }
-      delete tempObj[tripKey]
+  async function handleDelete(tripKey) {
 
-      setTrips(tempObj)
-      const userRef = doc(db, 'users', currentUser.uid, 'Trips', tripKey)
-      await deleteDoc(userRef)
+    const userRef = doc(db, 'users', currentUser.uid, 'Trips', tripKey)
+    await deleteDoc(userRef)
 
+    const tempObj = { ...trips }
+    delete tempObj[tripKey]
+    setTrips(tempObj)
+  }
+
+  function handleButton() {
+    if (open === false && trip !== '') {
+      setOpen(true);
+      setErr('')
+    } else {
+      setOpen(false);
+      setTrip('')
+      setArrDate(null)
+      setDepDate(null)
     }
   }
 
@@ -50,32 +63,38 @@ export default function UserDashboard() {
         <input type="text" placeholder='Enter trip' value={trip}
           onChange={(e) => setTrip(e.target.value)} className="outline-none p-3 
       text-base sm:text-lg text-slate-900 flex-1"/>
-        <Popup trigger={<button className='w-fit px-4 sm:px-6 py-2 sm:py-3 
+        <button onClick={() => handleButton()} id="addButton" className='w-fit px-4 sm:px-6 py-2 sm:py-3 
       bg-black text-white font-medium text-base duration-300 
       hover:opacity-40'>
           ADD
-        </button>}
+        </button>
+        <Popup open={open}
           position="relative"
           modal
           closeOnDocumentClick={false}
+          {...{ contentStyle, overlayStyle }}
+
         >
-          {close => (
-            <div className='flex flex-col items-center font-medium text-base rounded-lg w-full'>
-              <i onClick={close} className="text-3xl fa-solid fa-xmark cursor-pointer absolute top-0 right-2 "></i>
-              {/* <button onClick={close} className='absolute top-0 right-0 p-2'>CLOSE</button> */}
-              <div className='flex flex-col sm:flex-row space-x-20 pb-8 pt-8  '>
-                <div className="flex flex-col items-center">
-                  <h1>Choose arrival</h1>
-                  <DatePicker value={arrDate} onChange={(newValue) => setArrDate(newValue)} />
-                </div>
-                <div className="flex flex-col items-center">
-                  <h1>Choose departure</h1>
-                  <DatePicker value={depDate} onChange={(newValue) => setDepDate(newValue)} />
-                </div>
-              </div>
-              <button className='border w-1/2 bg-green-300' onClick={() => { handleAddTrip(); close(); }}>Create trip</button>
+
+          <div className='flex flex-col items-center font-medium text-base rounded-lg w-full'>
+            <i onClick={() => handleButton()} className="text-3xl fa-solid fa-xmark cursor-pointer absolute top-0 right-2 "></i>
+            {/* <button onClick={close} className='absolute top-0 right-0 p-2'>CLOSE</button> */}
+
+            <div className='flex text-red-600'>
+              <h1>{err}</h1>
             </div>
-          )}
+            <div className='flex flex-col sm:flex-row space-x-20 pb-8 pt-8  '>
+              <div className="flex flex-col items-center ">
+                <h1>Choose arrival</h1>
+                <DatePicker value={arrDate} onChange={(newValue) => setArrDate(newValue)} />
+              </div>
+              <div className="flex flex-col items-center">
+                <h1>Choose departure</h1>
+                <DatePicker value={depDate} onChange={(newValue) => setDepDate(newValue)} />
+              </div>
+            </div>
+            <button className='border w-1/2 bg-black text-white rounded-xl p-4' onClick={() => { handleAddTrip() }}>Create trip</button>
+          </div>
         </Popup>
 
 
