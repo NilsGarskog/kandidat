047c2383673ffd985fa7de452dbd79b29f5a3b37
Samuel Ram <samuel.gh.ram@gmail.com>
2023-04-13 13:08:02 +0200

    Calendar

--- components/Calendar.js
+++ components/Calendar.js
@@ -2,13 +2,9 @@ import React from 'react'
 
 export default function Calendar() {
 
-const itineary =[{day:1,id:0,slot:0},
-                {day:1, id:1, slot:1},
-                {day:1, id:2, slot:2},
-                {day:1, id:3, slot:3},
-                {day:1, id:4, slot:4},
-                {day:1, id:5, slot:5},
-                {day:2, id:6, slot:0}]
+const itineary =[{day:'mon', act:[32,3213,3,3,null,33]}]
+
+//id 1, 2, 3 reserverat för generiska aktiviteter ('lunch', 'dinner', 'activity')
 
   return (
     <div>Calendar</div>
