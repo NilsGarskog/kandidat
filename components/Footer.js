68d32827dd06d1e7111c20717302268e3f0495fb
IsakLindstrom <isak.lindstrom.7135@student.uu.se>
2023-04-14 09:11:40 +0200

    Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React,{ useState } from 'react'
 
 export default function Footer() {
+
+  const [envelopeClicked, setEnvelopeClicked] = useState(false)
+
+
   return (
     <div className='flex justify-center items-center gap-5 py-3'>
-        <i onclickclass="fa-regular fa-envelope duration-300 hover:opacity-30 cursor-pointer"></i>
+        <i onClick={() => setEnvelopeClicked(!envelopeClicked)} className="fa-regular fa-envelope duration-300 hover:opacity-30 cursor-pointer" > </i>
+        {envelopeClicked && <h2>travelplannerkandidat@gmail.com</h2>}
     </div>
+   
+   
   )
 }
