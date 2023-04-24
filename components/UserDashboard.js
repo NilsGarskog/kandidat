5efd477dfa4ee79cc48a4f2fd431608e84f80519
Samuel Ram <samuel.gh.ram@gmail.com>
2023-04-24 10:44:18 +0200

    Fix

--- components/UserDashboard.js
+++ components/UserDashboard.js
@@ -62,10 +62,11 @@ export default function UserDashboard() {
     setTrips({ ...trips, [newKey]: trip })
     const userRef = doc(db, 'users', currentUser.uid, 'Trips', newKey.toString())
 
-    await setDoc(userRef, { Name: trip, arrDate: arrDate.format('YYYY-MM-DD'), depDate: depDate.format('YYYY-MM-DD'), tripImageUrl: url, preferredImageIndex: 0 })
+    await setDoc(userRef, { Name: trip, arrDate: arrDate.format('YYYY-MM-DD'), depDate: depDate.format('YYYY-MM-DD'), tripImageUrl: url, preferredImageIndex: 0 }) 
 
     handleButton(false)
 
+    
 
   }
 
