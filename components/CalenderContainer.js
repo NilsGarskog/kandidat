de803bfbc7651181543a036a73ece13c8cb9dabb
NilsGarskog <93979268+NilsGarskog@users.noreply.github.com>
2023-05-09 14:21:24 +0200

    Fackmannen

--- components/CalenderContainer.js
+++ components/CalenderContainer.js
@@ -2,18 +2,24 @@ import useFetchAct from '@/hooks/FetchActivities'
 import useFetchTripData from '../hooks/FetchTripData'
 import React, { useEffect, useState } from 'react'
 import Calendar from '@/components/Calendar';
+import { useRouter } from "next/router";
 
-// async function regenerate(regen, setRegen) {
-//     if (regen === true) {
-//         await setRegen(false)
-//     }
-//     setRegen(true)
-// }
+async function regenerate(regen, setRegen, router, tripKey) {
+
+    if (regen === true) {
+        await setRegen(false)
+    }
+    await setRegen(true)
+
+    // setTimeout(() => { window.location.href = `/${tripKey}`; }, 1000);
+}
 
 
 export default function ActivityContainer(props) {
     let [regen, setRegen] = useState(false)
+    const router = useRouter();
     let algoData = props.data
+    const tripKey = algoData.tripKey
     const activityData = useFetchAct(algoData.tripKey)
     const activities = activityData.actArr
 
@@ -22,8 +28,8 @@ export default function ActivityContainer(props) {
         return (
             <div className='flex items-center flex-col'>
                 <Calendar data={algoData} regen={regen}></Calendar>
-                {/* <i onClick={() => regenerate(regen, setRegen)} className="fa-solid duration-300 hover:rotate-90 fa-arrows-rotate text-6xl -mt-40 cursor-pointer"></i> */}
-            </div>
+                <i onClick={() => { regenerate(regen, setRegen, router, tripKey) }} className="fa-solid duration-300 hover:rotate-90 fa-arrows-rotate text-6xl -mt-40 cursor-pointer"></i>
+            </div >
         )
     } else {
         return (
