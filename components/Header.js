36587efa849d0acd65abbeb381d06e46a48d772e
Samuel Ram <samuel.gh.ram@gmail.com>
2023-05-11 14:20:42 +0200

    Header profile image

--- components/Header.js
+++ components/Header.js
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from "react";
 import Modal from "./Modal";
 import { useAuth } from "../context/authContext";
 import { useRouter } from "next/router";
+import useFetchProfileInfo from "@/hooks/FetchProfileInfo";
 
 export default function Header() {
   const [openModal, setOpenModal] = useState(false);
   const { currentUser } = useAuth();
+  const { loadingPInfo, errorPInfo, profileData, setProfileData } = useFetchProfileInfo()
   const router = useRouter();
+  console.log(profileData)
+
 
   function closeModalOnClickOutside(event) {
     console.log(event.target.className)
@@ -34,7 +38,16 @@ export default function Header() {
         <h1 className='text-2xl select-none text-black   sm:text-4xl font-medium'> TRAPLA </h1>
         <img className='h-12 pl-2' src='../img/logo.svg'/>
         </div>
-        <i onClick={() => setOpenModal(true)} className="fa-solid fa-user text-xl text-black duration-300 hover:opacity-40 cursor-pointer sm:text-3xl"></i>
+
+       {profileData?.ProfileInfo?.ProfileImageURL ?  <div className="sm:ml-4 ml-0 sm:mt-0 mt-2 sm:h-12 sm:w-12 h-40 w-40 rounded-full overflow-hidden cursor-pointer">
+                        <img onClick={() => {
+                        setOpenModal(true);
+     }}
+                          className="w-full h-full object-cover "
+                          src=
+                            {profileData.ProfileInfo.ProfileImageURL}
+                          
+                        ></img> </div>:  <i onClick={() => setOpenModal(true)} className="fa-solid fa-user text-xl text-black duration-300 hover:opacity-40 cursor-pointer sm:text-3xl"></i>}
       </div>
     </>
   );
