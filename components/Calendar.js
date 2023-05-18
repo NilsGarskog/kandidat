29c2b33bf227f2893cf37428d65f19f59f164cc6
IsakLindstrom <isak.lindstrom.7135@student.uu.se>
2023-05-18 13:34:08 +0200

    några test

--- components/Calendar.js
+++ components/Calendar.js
@@ -1,86 +1,106 @@
-import React, { useState, useEffect } from 'react'
+// Calendar.js
+
+import React, { useState, useEffect } from 'react';
 import Algoritmen from '@/Algorithms/Algoritmen';
 import { removeDuplicates, getActName } from 'utils/calUtils.js';
 import Day from './Day';
 import useFetchTripData from '../hooks/FetchTripData'
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 export default function Calendar(props) {
-  const data = props.data
-  const itCreated = props.itCreated
-  const regen = props.regen
-  let itineary = []
+  const data = props.data;
+  const itCreated = props.itCreated;
+  const regen = props.regen;
+  let itineary = [];
+
   if (data.itineary === null || regen != null) {
-    itineary = Algoritmen(data.arrDate, data.depDate, data.actArr)
+    itineary = Algoritmen(data.arrDate, data.depDate, data.actArr);
   } else {
-    itineary = data.itineary
+    itineary = data.itineary;
   }
-  let allData = useFetchTripData(data.tripKey)
+
+  let allData = useFetchTripData(data.tripKey);
 
   const [currentView, setCurrentView] = useState(0);
   const [daysPerView, setDaysPerView] = useState(3);
 
-  const isMobile = window.innerWidth < 640; // adjust breakpoint as needed
+  const isMobile = window.innerWidth < 640;
 
   useEffect(() => {
     if (isMobile) {
-      setDaysPerView(1); // adjust number of days per view for mobile
+      setDaysPerView(1);
     } else {
-      setDaysPerView(3); // set back to default for larger screens
+      setDaysPerView(3);
     }
   }, [isMobile]);
+
+  const handleActivityDrop = (dayIndex, activityIndex, droppedActivity) => {
+    const updatedItineary = [...itineary];
+    updatedItineary[dayIndex].act[activityIndex] = droppedActivity;
+    setItineary(updatedItineary);
+  };
 
   const daysToRender = itineary.slice(currentView * daysPerView, (currentView * daysPerView) + daysPerView);
   const isNextEnabled = (currentView + 1) * daysPerView < itineary.length;
   const isPrevEnabled = currentView > 0;
+
   if (allData.loading === false) {
-    itineary = allData.tripData.itineary
+    itineary = allData.tripData.itineary;
+
     return (
-      <div className='flex flex-col'>
-        {!isMobile && <div>
-          <h1 className='uppercase text-center text-7xl -mb-10 mt-4 font-bold cursor-default select-none'>
-            YOUR Itinerary
-          </h1>
-        </div>}
-        <div className={`flex w-full mt-8 sm:mt-20`}>
-
-          {itineary.length > daysPerView + 1 && <div className='flex  mt-0 justify-evenly'>
-
-            <button className='h-20 px-0' onClick={() => setCurrentView(currentView - 1)} disabled={!isPrevEnabled}>
-              <img className={`absolute left-0 sm:mt-32 mt-40 ml-6 sm:ml-40 h-12 sm:h-20 ${!isPrevEnabled ? 'opacity-50 ' : 'cursor-pointer opacity-80 hover:opacity-100'}`}
-                src='../icons/arrow-left.png'
-                style={{ backfaceVisibility: 'hidden' }}
-              />
-            </button>
-          </div>
-          }
-
-
-          <div className="w-full h-[70ch] flex justify-center select-none cursor-default">
-
-            {daysToRender.map((item, index) => (
-              <Day
-                key={item.day}
-                day={item.day}
-                activities={item.act}
-                actArr={data.actArr}
-              />
-            ))}
-
-            {itineary.length > daysPerView + 1 && <div className='flex  mt-0 justify-evenly'>
-
-              <button className='h-20  px-0' onClick={() => setCurrentView(currentView + 1)} disabled={!isNextEnabled}>
-                <img className={`absolute right-0 sm:mt-32 mt-40 mr-6 sm:mr-40 h-12 sm:h-20 px-0 ${!isNextEnabled ? 'opacity-50 ' : 'cursor-pointer opacity-80 hover:opacity-100'}`}
-                  src='../icons/arrow-right.png'
-                  style={{ backfaceVisibility: 'hidden' }}
-                />
-              </button>
+      <DndProvider backend={HTML5Backend}>
+        <div className='flex flex-col'>
+          {!isMobile && (
+            <div>
+              <h1 className='uppercase text-center text-7xl -mb-10 mt-4 font-bold cursor-default select-none'>
+                YOUR Itinerary
+              </h1>
             </div>
-            }
+          )}
+          <div className={`flex w-full mt-8 sm:mt-20`}>
+            {itineary.length > daysPerView + 1 && (
+              <div className='flex  mt-0 justify-evenly'>
+                <button className='h-20 px-0' onClick={() => setCurrentView(currentView - 1)} disabled={!isPrevEnabled}>
+                  <img
+                    className={`absolute left-0 sm:mt-32 mt-40 ml-6 sm:ml-40 h-12 sm:h-20 ${!isPrevEnabled ? 'opacity-50 ' : 'cursor-pointer opacity-80 hover:opacity-100'}`}
+                    src='../icons/arrow-left.png'
+                    style={{ backfaceVisibility: 'hidden' }}
+                  />
+                </button>
+              </div>
+            )}
+
+            <div className="w-full h-[70ch] flex justify-center select-none cursor-default">
+              {daysToRender.map((item, index) => (
+                <Day
+                  key={item.day}
+                  day={item.day}
+                  activities={item.act}
+                  actArr={data.actArr}
+                  onActivityDrop={(activityIndex, droppedActivity) =>
+                    handleActivityDrop(index + currentView * daysPerView, activityIndex, droppedActivity)
+                  }
+                />
+              ))}
+
+              {itineary.length > daysPerView + 1 && (
+                <div className='flex  mt-0 justify-evenly'>
+                  <button className='h-20  px-0' onClick={() => setCurrentView(currentView + 1)} disabled={!isNextEnabled}>
+                    <img
+                      className={`absolute right-0 sm:mt-32 mt-40 mr-6 sm:mr-40 h-12 sm:h-20 px-0 ${!isNextEnabled ? 'opacity-50 ' : 'cursor-pointer opacity-80 hover:opacity-100'}`}
+                      src='../icons/arrow-right.png'
+                      style={{ backfaceVisibility: 'hidden' }}
+                    />
+                  </button>
+                </div>
+              )}
+            </div>
           </div>
-
-
         </div>
-      </div>
-    )
-  } else { return <h1></h1> }
+      </DndProvider>
+    );
+  } else {
+    return <h1></h1>;
+  }
 }
--- components/Day.js
+++ components/Day.js
@@ -1,48 +1,102 @@
-import React from 'react'
-import { removeDuplicates, getActName } from 'utils/calUtils.js';
 
+import { removeDuplicates, getActName } from 'utils/calUtils.js';
+import { useDrag, useDrop } from 'react-dnd';
+import React, { useState, useEffect } from 'react';
 
 export default function Day(props) {
-    const {day, activities, actArr} = props
-    let newActivityList = []
-    let newArray = removeDuplicates(activities)
-  
-    newArray.forEach((activity, index) => {
-      let actName = getActName(activity, actArr).name
-      let actLength = getActName(activity, actArr).length
-      let actType = getActName(activity, actArr).type
-  
-      if (activity === 1) {
-        actName = 'LUNCH'
-        actLength = 0
-        actType = 1
-      }
-      else if (activity === 2) {
-        actName = 'DINNER'
-        actLength = 0
-        actType = 2
-      }
-      else if (activity === 3 || activity === null) {
-        actName = 'FREE TIME!'
-        actLength = 0
-        actType = 0
-      }
-  
-      newActivityList.push(
-        <div
-          id={activity}
-          className={`font-medium flex items-center justify-center rounded-xl w-[15ch] ${actLength == 0 ? 'h-[5ch]' : 'h-[11ch]'} text-lg mt-3 text-slate-900 text-center ${actType == 1 || actType == 2 ? 'bg-calYellow' : 'bg-calBlue'}`}
-          key={index}
-        >
-          {actName}
-        </div>
-      )
-    })
-  
-    return (
-      <div className="flex flex-col align-center px-6">
-        <h3 className="uppercase text-xl text-center font-light">{day}</h3>
-        {newActivityList}
+  const { day, activities, actArr } = props;
+  const [updatedActivities, setUpdatedActivities] = useState(activities);
+
+  const handleDrop = (index, item) => {
+    // Retrieve the dragged activity and its index
+    const draggedIndex = item.index;
+    const draggedActivity = item.activity;
+
+    // Retrieve the dropped activity and its index
+    const droppedIndex = index;
+    const droppedActivity = activities[droppedIndex];
+
+    // Swap the activities in the array
+    const updatedActivities = [activities];
+    updatedActivities[draggedIndex] = droppedActivity;
+    updatedActivities[droppedIndex] = draggedActivity;
+
+    // Update the state or perform other actions to reflect the changes
+    // For example, you can update the activities state
+    setUpdatedActivities(updatedActivities);
+
+    // You can also perform any other necessary actions based on the swapped activities
+
+    console.log('Swapped Activities:', updatedActivities);
+  };
+
+  const newArray = removeDuplicates(activities);
+
+  return (
+    <div className="flex flex-col align-center px-6">
+      <h3 className="uppercase text-xl text-center font-light">{day}</h3>
+      {newArray.map((activity, index) => {
+        let actName = getActName(activity, actArr).name;
+        let actLength = getActName(activity, actArr).length;
+        let actType = getActName(activity, actArr).type;
+
+        if (activity === 1) {
+          actName = 'LUNCH';
+          actLength = 0;
+          actType = 1;
+        } else if (activity === 2) {
+          actName = 'DINNER';
+          actLength = 0;
+          actType = 2;
+        } else if (activity === 3 || activity === null) {
+          actName = 'FREE TIME!';
+          actLength = 0;
+          actType = 0;
+        }
+
+        return (
+          <Activity
+            key={index}
+            index={index}
+            activity={activity}
+            actName={actName}
+            actLength={actLength}
+            actType={actType}
+            handleDrop={handleDrop}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+function Activity({ index, activity, actName, actLength, actType, handleDrop }) {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: 'activity',
+    item: { index, activity },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  }));
+
+  const [, drop] = useDrop({
+    accept: 'activity',
+    drop: (item) => handleDrop(index, item),
+  });
+
+  return (
+    <div
+      ref={drop}
+      id={activity}
+      className={`font-medium hover:scale-105 hover:duration-300 duration-1000 flex items-center justify-center rounded-xl w-[15ch] ${
+        actLength === 0 ? 'h-[5ch]' : 'h-[11ch]'
+      } text-lg mt-3 text-slate-900 text-center ${
+        actType === 1 || actType === 2 ? 'bg-calYellow' : 'bg-calBlue'
+      } ${isDragging ? 'opacity-50' : 'opacity-100'}`}
+    >
+      <div ref={drag} style={{ cursor: 'grab' }}>
+        {actName}
       </div>
-    )
-  }
+    </div>
+  );
+}
